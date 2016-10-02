af41f5bd28eb945aa23a6ed34aa753c4a87c95da
KDMEugene <KDMEugene@users.noreply.github.com>
2016-10-02 19:03:31 +0300

    Commitishe

---
+++ TO TEST THINGS OUT/PJ 1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PJ_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("YA HZ");
+            Console.Read();
+        }
+    }
+}
